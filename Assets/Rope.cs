4dc03b13a6fbfdf22d0e25e67dea5f3e1eb2cc7c
Elijah James <uniquecorn@rapbotstudios.com>
2020-01-23 16:44:39 +0800

    Initial commit

---
+++ Assets/Rope.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    private NativeArray<RopeSegment> ropeSegments;
+    private float segmentLength = 0.1f;
+    private int segments = 35;
+    public Transform target;
+    public bool useTarget;
+    Vector3[] ropePositions;
+    // Use this for initialization
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        Vector3 ropePoint = transform.position;
+        ropeSegments = new NativeArray<RopeSegment>(segments, Allocator.Persistent);
+        for (int i = 0; i < segments; i++)
+        {
+            ropeSegments[i] = new RopeSegment(ropePoint);
+            ropePoint.y -= segmentLength;
+        }
+        ropePositions = new Vector3[segments];
+    }
+
+    public void RopeUpdate()
+    {
+        Render();
+    }
+
+    public void RopeFixedUpdate()
+    {
+        SimulateBurst();
+    }
+
+    private void SimulateBurst()
+    {
+        IRopeSimulate ropeSimulation = new IRopeSimulate
+        {
+            fixedDeltaTime = Time.fixedDeltaTime,
+            segments = ropeSegments,
+            pos = (Vector2)transform.position
+        };
+        JobHandle x = ropeSimulation.Schedule(ropeSegments.Length, 16);
+        IConstraintJob constraintSim = new IConstraintJob
+        {
+            segments = ropeSegments,
+            pos = (Vector2)transform.position,
+            pos2 = (Vector2)target.transform.position,
+            useTarget = this.useTarget,
+            segmentLength = this.segmentLength
+        };
+        constraintSim.Schedule(x).Complete();
+    }
+    void OnDestroy()
+    {
+        ropeSegments.Dispose();
+    }
+
+    private void Render()
+    {
+        for (int i = 0; i < segments; i++)
+        {
+            ropePositions[i] = ropeSegments[i].Pos();
+        }
+        if (!useTarget)
+        {
+            target.transform.position = ropeSegments[ropeSegments.Length - 1].Pos();
+        }
+
+        lineRenderer.positionCount = ropePositions.Length;
+        lineRenderer.SetPositions(ropePositions);
+    }
+
+    public struct RopeSegment
+    {
+        public float2 posNow;
+        public float2 posOld;
+
+        public RopeSegment(Vector2 pos)
+        {
+            posNow = pos;
+            posOld = pos;
+        }
+        public Vector3 Pos()
+        {
+            return new Vector3(posNow.x, posNow.y, 0);
+        }
+    }
+    [BurstCompile(CompileSynchronously = true)]
+    public struct IRopeSimulate : IJobParallelFor
+    {
+        public float2 pos, pos2;
+        public NativeArray<RopeSegment> segments;
+        public float fixedDeltaTime;
+        public void Execute(int index)
+        {
+            float2 gravity = new float2(0, -1);
+            RopeSegment segment = segments[index];
+            float2 velocity = segment.posNow - segment.posOld;
+            segment.posOld = segment.posNow;
+            segment.posNow += velocity;
+            segment.posNow += gravity * fixedDeltaTime;
+            segments[index] = segment;
+        }
+    }
+    [BurstCompile(CompileSynchronously = true)]
+    public struct IConstraintJob : IJob
+    {
+        public float2 pos, pos2;
+        public NativeArray<RopeSegment> segments;
+        public bool useTarget;
+        public float segmentLength;
+        public void Execute()
+        {
+            RopeSegment firstSegment = this.segments[0];
+            firstSegment.posNow = new float2(pos.x, pos.y);
+            segments[0] = firstSegment;
+            if (useTarget)
+            {
+                RopeSegment endSegment = this.segments[segments.Length - 1];
+                endSegment.posNow = new float2(pos2.x, pos2.y);
+                segments[segments.Length - 1] = endSegment;
+            }
+            for (int j = 0; j < 50; j++)
+            {
+                for (int i = 0; i < segments.Length - 1; i++)
+                {
+                    RopeSegment firstSeg = segments[i];
+                    RopeSegment secondSeg = segments[i + 1];
+                    float2 dir = firstSeg.posNow - secondSeg.posNow;
+                    float dist = math.sqrt(dir.x * dir.x + dir.y * dir.y);
+                    float error = math.abs(dist - segmentLength);
+                    float2 changeDir = float2.zero;
+                    if (dist > segmentLength)
+                    {
+                        changeDir = math.normalize(firstSeg.posNow - secondSeg.posNow);
+                    }
+                    else if (dist < segmentLength)
+                    {
+                        changeDir = math.normalize(secondSeg.posNow - firstSeg.posNow);
+                    }
+                    float2 changeAmount = changeDir * error;
+                    if (i > 0)
+                    {
+                        firstSeg.posNow -= changeAmount * 0.5f;
+                        this.segments[i] = firstSeg;
+                    }
+                    if ((i + 1 < segments.Length - 1) || !useTarget)
+                    {
+                        secondSeg.posNow += changeAmount * 0.5f;
+                        this.segments[i + 1] = secondSeg;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/RopeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeManager : MonoBehaviour
+{
+    public List<Rope> ropes;
+    public TMPro.TextMeshProUGUI lineText;
+    private void FixedUpdate()
+    {
+        if (ropes != null)
+        {
+            for (int i = 0; i < ropes.Count; i++)
+            {
+                ropes[i].RopeFixedUpdate();
+            }
+        }
+    }
+    private void Update()
+    {
+        if (ropes != null)
+        {
+            for (int i = 0; i < ropes.Count; i++)
+            {
+                ropes[i].RopeUpdate();
+            }
+        }
+    }
+
+    public void AddRope()
+    {
+        ropes.Add(Instantiate(ropes[0], new Vector3(Random.Range(-5, 5f), Random.Range(-5, 5f), 0), Quaternion.identity));
+        ropes[ropes.Count - 1].target.transform.position = new Vector3(Random.Range(-5, 5f), Random.Range(-5, 5f), 0);
+        ropes[ropes.Count - 1].transform.name = ropes.Count.ToString();
+        lineText.text = ropes.Count.ToString();
+    }
+}
